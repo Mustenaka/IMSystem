0fe166ed6e9b55226a73652c9baaa71c8135e115
Mustenaka <Mumten120@outlook.com>
2022-03-02 18:43:02 +0800

    Update main function

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"imSystem/server"
 )
 
 func main() {
+	fmt.Println("Start Program.")
+
 	ser := server.NewServer("127.0.0.1", 8888)
 	ser.Start()
+
+	fmt.Println("done.")
 }
